0971b4daa4842ac3d4076d8990e08c671724695c
John Costanzo <jrock2004@gmail.com>
2015-11-19 19:31:37 -0500

    Commenting out macvim

--- install/brew.sh
+++ install/brew.sh
@@ -20,7 +20,7 @@ brew install dnsmasq
 # development tools
 brew install git
 brew install hub
-brew install macvim --override-system-vim
+#brew install macvim --with-override-system-vim
 brew install reattach-to-user-namespace
 brew install tmux
 brew install zsh
