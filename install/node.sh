edbfee359c94c8eea3782378e29cbc5eb507a296
John Costanzo <jrock2004@gmail.com>
2016-01-08 22:36:05 -0500

    Adding jshint and adding apps to install via node

---
+++ install/node.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm install -g gulp bower browser-sync yo nodemon express-generator
+npm install -g eslint jscs jshint jsxhint jsonlint tsc csslint handlebars
+npm install -g jade-lint less phplint sass tslint ember-cli csslint
+npm install -g generator-aspnet
