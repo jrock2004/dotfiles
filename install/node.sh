55311d2909a7fa0a5f9d2098c95fab19fd9a72ab
John Costanzo <jrock2004@gmail.com>
2016-05-23 14:18:02 -0400

    Updating the node modules

--- install/node.sh
+++ install/node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm install -g gulp bower browser-sync yo nodemon express-generator
-npm install -g eslint jscs jshint jsxhint jsonlint tsc csslint handlebars
+npm install -g eslint jscs jshint jsxhint jsonlint typescript csslint handlebars
 npm install -g jade-lint less phplint sass tslint ember-cli csslint
-npm install -g generator-aspnet ember-cli-azure-deploy watchman
+npm install -g generator-aspnet ember-cli-azure-deploy watchman firebase-tools
