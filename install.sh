889d7fd19cf9fcc3ca163a986742cdb83468a3ac
John Costanzo <jrock2004@gmail.com>
2016-08-22 10:42:18 -0400

    Updating some things

--- install.sh
+++ install.sh
@@ -9,91 +9,26 @@ OS=""
 echo "Symlinking dotfiles"
 source install/link.sh
 
-if [ "$(uname)" == "Linux" ]; then
-	echo "Running Linux"
-
-	# Set up SSH keys
-	while true; do
-		read -p "Copy your key to github: " yn
-		case $yn in
-			[Yy]* )
-				ssh-keygen -t rsa -b 4096 -C $EMAIL
-				eval "$(ssh-agent -s)"
-				ssh-add ~/.ssh/id_rsa
-				xclip -sel clip < ~/.ssh/id_rsa.pub
-				break
-				;;
-			[Nn]* )
-				break
-				;;
-		esac
-	done
-fi
+echo "Creating needed directories"
+mkdir -p $DEVFOLDER
+mkdir -p $NPMFOLDER
 
 if [ "$(uname)" == "Darwin" ]; then
-	echo "Running OSX"
-
-	# Set up SSH keys
-	while true; do
-		read -p "Copy your key to github: " yn
-		case $yn in
-			[Yy]* )
-				ssh-keygen -t rsa -b 4096 -C $EMAIL
-				eval "$(ssh-agent -s)"
-				ssh-add ~/.ssh/id_rsa
-				pbcopy < ~/.ssh/id_rsa.pub
-				break
-				;;
-			[Nn]* )
-				break
-				;;
-		esac
-	done
-fi
-
-if [ "$(uname)" == "Darwin" ]; then
-	echo "Brewing all the things"
 	source install/brew.sh
-
-	echo "Updating OSX settings"
 	source install/osx.sh
+	source install/nvm.sh
+	source install/python.sh
+	source install/node.sh
+	
 
 	echo "Configuring nginx"
 	mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.original
 	ln -s ~/.dotfiles/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
 fi
 
-if [ "$(uname)" == "Linux" ]; then
-	# Lets get the linux distro
-	OS=$(cat /etc/os-release | grep -sw NAME)
-	NEWOS=$(echo "$OS" | cut -d \" -f2)
-	
-	case "$NEWOS" in
-		*Arch*)
-			source install/arch.sh
-			npm config set prefix $NPMFOLDER
-			sudo ln -s /usr/bin/pip2 /usr/bin/pip
-			;;
-		*Ubuntu*)
-			source install/debian.sh
-			npm config set prefix $NPMFOLDER
-			sudo ln -s /usr/bin/pip2 /usr/bin/pip
-			;;
-	esac
-fi
-
-echo "Creating needed directories"
-mkdir -p $DEVFOLDER
-mkdir -p $NPMFOLDER
-
 echo "Installing Node Apps"
 source install/node.sh
 
-echo "Install some Python stuff"
-source install/python.sh
-
-source ~/.bashrc
-source ~/.bash_profile
 
 echo "Installing and setting Ruby version"
 rbenv install 2.2.3
