f4e065d667b2c9e193150ac0ab32bf5edaed9cef
John Costanzo <jrock2004@gmail.com>
2016-11-02 11:19:27 -0400

    Adding mono and .net core

--- install/bashwindows.sh
+++ install/bashwindows.sh
@@ -2,12 +2,24 @@
 
 echo "Lets install our Linux Stuff"
 
+# Sources for Neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+# Sources for .net core
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+# Sources for Mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee
+/etc/apt/sources.list.d/mono-xamarin.list
+
+# Lets updated so we can get new sources
 sudo apt-get update
 
-sudo apt-get install -y bash-completion vim vim-scripts python-dev python-pip python3-dev python3-pip neovim build-essential ack-grep tree wget nginx ruby-build tmux markdown irssi irssi-scripts zsh
+sudo apt-get install -y bash-completion vim vim-scripts python-dev python-pip python3-dev python3-pip neovim
+build-essential ack-grep tree wget nginx ruby-build tmux markdown irssi irssi-scripts zsh xclip cmake
+dotnet-dev-1.0.0-preview2-003131 mono-complete
 
 # Install some extra tools
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
