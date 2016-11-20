668bca0d31773cd2fef04a317afcb946c579a26b
John Costanzo <jrock2004@users.noreply.github.com>
2016-11-20 10:35:45 -0500

    Install php composer and laravel

---
+++ install/php.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get -y install php-cli unzip php-mysql php-mcrypt php-mbstring
+
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo chown -R jcostanzo:jcostanzo $HOME/.composer
+
+rm composer-setup.php
+
+# Install Laravel
+$HOME/.composer/vendor/bin/composer global require "laravel/installer"
