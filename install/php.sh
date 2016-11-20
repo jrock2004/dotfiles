0808dcc5cc23c134bba749609817a2989b151f28
John Costanzo <jrock2004@users.noreply.github.com>
2016-11-20 11:22:53 -0500

    Adding php xml dependency

--- install/php.sh
+++ install/php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install php-cli unzip php-mysql php-mcrypt php-mbstring
+sudo apt-get -y install php-cli unzip php-mysql php-mcrypt php-mbstring php-xml
 
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
