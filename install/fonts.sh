73b34f5fea1729b828e7f076ff3347b296beb8d5
John Costanzo <jrock2004@users.noreply.github.com>
2016-12-20 18:49:17 -0500

    Fixes missing sudo

--- install/fonts.sh
+++ install/fonts.sh
@@ -3,14 +3,15 @@
 ### FiraCode
 git clone https://github.com/tonsky/FiraCode.git $HOME/bin/fira
 sudo mkdir -p /usr/share/fonts/truetype/firaCode
-cp -a $HOME/bin/fira/distr/ttf/*.ttf /usr/share/fonts/truetype/firaCode/
-
-# Cleanup downloaded fonts
-rm -Rf $HOME/bin/fira
+sudo cp -a $HOME/bin/fira/distr/ttf/*.ttf /usr/share/fonts/truetype/firaCode/
 
 ### Powerline patched fonts
 git clone https://github.com/powerline/fonts.git $HOME/bin/powerline
 sh $HOME/bin/powerline/install.sh
+
+# Cleanup downloaded fonts
+rm -Rf $HOME/bin/fira
+rm -Rf $HOME/bin/powerline
 
 # Regenerate fonts
 fc-cache -f -v
