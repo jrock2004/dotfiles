7492c860b240e1c6cbde64660b10220a4c104030
John Costanzo <jrock2004@gmail.com>
2017-03-04 10:54:28 -0500

    Fixing indenting

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 command_exists() {
-    type "$1" > /dev/null 2>&1
+	type "$1" > /dev/null 2>&1
 }
 
 # Setting up some variables
@@ -41,40 +41,40 @@ source install/php.sh
 
 # Setup SSH key if needed
 if [ ! -f ~/.ssh/id_rsa.pub  ]; then
-    mkdir ~/.ssh
-    ssh-keygen -t rsa -b 4096 -C "$EMAIL"
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
+	mkdir ~/.ssh
+	ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_rsa
 
-    GITHUB_SSH_URL=https://github.com/settings/ssh
+	GITHUB_SSH_URL=https://github.com/settings/ssh
 
-    if command_exists xdg-open; then
-        xdg-open $GITHUB_SSH_URL
-    else
-        echo $GITHUB_SSH_URL
-    fi
+	if command_exists xdg-open; then
+		xdg-open $GITHUB_SSH_URL
+	else
+		echo $GITHUB_SSH_URL
+	fi
 
-    cat $HOME/.ssh/id_rsa.pub
+	cat $HOME/.ssh/id_rsa.pub
 
-    read -p "\n\nHit ENTER after adding to Github\n\n"
+	read -p "\n\nHit ENTER after adding to Github\n\n"
 else
-    echo ".ssh directory already exists, not generating"
+	echo ".ssh directory already exists, not generating"
 fi
 
 if ! command_exists zplug; then
-    echo "installing zplug, a plugin manager for zsh - http://zplug.sh"
-    # curl -sL zplug.sh/installer | zsh
-    git clone https://github.com/zplug/zplug.git ~/.zplug
+	echo "installing zplug, a plugin manager for zsh - http://zplug.sh"
+	# curl -sL zplug.sh/installer | zsh
+	git clone https://github.com/zplug/zplug.git ~/.zplug
 fi
 
 # Setting env to zsh instead of bash
 echo "Switching to ZSH"
 if ! command_exists zsh; then
-    echo "zsh not found. Please install and then re-run installation scripts"
-    exit 1
+	echo "zsh not found. Please install and then re-run installation scripts"
+	exit 1
 elif ! [[ $SHELL =~ .*zsh.* ]]; then
-    echo "Configuring zsh as default shell"
-    chsh -s $(which zsh)
+	echo "Configuring zsh as default shell"
+	chsh -s $(which zsh)
 fi
 
 echo "Done. Close window and re-open to enjoy"
