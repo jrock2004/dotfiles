3324b4ec0b270bd58fc3c242935400852eddb5a9
John Costanzo <jrock2004@gmail.com>
2017-04-25 09:38:29 -0400

    Adding new apps to install and fixing tmux for new version

--- install/linux.sh
+++ install/linux.sh
@@ -8,10 +8,14 @@ command_exists() {
 sudo pacman -S --noconfirm bash-completion vim python python-pip neovim \
 python-neovim python2-neovim ack tree wget nginx tmux markdown irssi zsh \
 xclip cmake mono firefox extra/ffmpeg obs-studio autoconf bison community/openshot \
-ngrep highlight wine winetricks virtualbox docker-compose libffi libyaml \
-openssl zlib composer
+ngrep highlight wine-staging winetricks virtualbox docker-compose libffi libyaml \
+openssl zlib composer clang
 
-yaourt -Sy spotify silver-searcher-git scudcloud visual-studio-code
+gpg --recv-keys --keyserver hkp://pgp.mit.edu D9C4D26D0E604491
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 5CC908FDB71E12C2
+
+yaourt -Sy spotify silver-searcher-git visual-studio-code dropbox nautilus-dropbox
+libopenssl-1.0-compat libcurl-openssl-1.0 lib32-libldap lib32-gnutls
 
 # Install some extra tools
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
