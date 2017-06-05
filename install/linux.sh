851faf6a7b223f9f561a6cbf0e102fe2077b1fb6
John Costanzo <jrock2004@gmail.com>
2017-06-04 22:20:20 -0400

    Cleaning up after install

--- install/linux.sh
+++ install/linux.sh
@@ -7,9 +7,9 @@ command_exists() {
 
 sudo pacman -S --noconfirm bash-completion vim python python-pip neovim \
 python-neovim python2-neovim ack tree wget nginx tmux markdown irssi zsh \
-xclip cmake mono firefox extra/ffmpeg obs-studio autoconf bison community/openshot \
-ngrep highlight winetricks virtualbox docker-compose libffi libyaml \
-openssl zlib composer clang
+xclip cmake mono firefox extra/ffmpeg obs-studio autoconf bison \
+community/openshot ngrep highlight winetricks virtualbox docker-compose \
+libffi libyaml openssl zlib composer clang
 
 gpg --recv-keys --keyserver hkp://pgp.mit.edu D9C4D26D0E604491
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 5CC908FDB71E12C2
@@ -17,7 +17,6 @@ gpg --recv-keys --keyserver hkp://pgp.mit.edu 5CC908FDB71E12C2
 yaourt -Sy spotify silver-searcher-git visual-studio-code dropbox \
 nautilus-dropbox libopenssl-1.0-compat libcurl-openssl-1.0 lib32-libldap \
 lib32-gnutls
-
 
 # Install some extra tools
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
@@ -33,6 +32,8 @@ git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-buil
 
 # Get Tmux spotify client
 sudo curl https://raw.githubusercontent.com/jrock2004/tmux-spotify/master/tmux-spotify -o /usr/local/bin/tmux-spotify
+
+sudo chmod +x /usr/local/bin/tmux-spotify
 
 # Setting some dconf settings
 
@@ -56,3 +57,4 @@ gsettings set org.gnome.gnome-screenshot default-file-type jpg
 
 # Set auto timezone
 gsettings set org.gnome.desktop.datetime automatic-timezone true
+
