8575aec9381ab0b52c93b4f8972f1efb8f1c31ad
John Costanzo <jrock2004@gmail.com>
2017-06-09 16:46:06 -0400

    Removing dep that are not needed and adding new one

--- install/node.sh
+++ install/node.sh
@@ -3,6 +3,6 @@
 npm install -g gulp bower browser-sync yo nodemon express-generator \
 	eslint jscs jshint jsxhint jsonlint typescript csslint handlebars \
 	jade-lint less phplint sass tslint ember-cli csslint \
-	generator-aspnet ember-cli-azure-deploy watchman firebase-tools \
-	diff-so-fancy git-standup
+	generator-aspnet ember-cli-azure-deploy firebase-tools \
+	diff-so-fancy git-standup preact-cli
 
