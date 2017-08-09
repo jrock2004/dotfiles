0d6bae9521e13db58c6fddbd3e0e8195439e9f66
John Costanzo <jrock2004@gmail.com>
2017-08-09 09:53:38 -0400

    Fixing zplug load errors

---
+++ zsh/zplug.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+zplug 'zsh-users/zsh-syntax-highlighting', defer:2
+zplug 'zsh-users/zsh-autosuggestions'
+zplug 'akoenig/npm-run.plugin.zsh'
+
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+zplug load
