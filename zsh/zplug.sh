d5ecae8b918be42f5de7db99c71cdbb041ba958f
John Costanzo <jrock2004@gmail.com>
2018-02-17 14:18:15 -0500

    Adding docker compose completion

--- zsh/zplug.sh
+++ zsh/zplug.sh
@@ -4,6 +4,7 @@ zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'zsh-users/zsh-autosuggestions'
 zplug 'akoenig/npm-run.plugin.zsh'
+zplug "docker/compose", use:contrib/completion/zsh
 
 if ! zplug check --verbose; then
   printf "Install? [y/N]: "
