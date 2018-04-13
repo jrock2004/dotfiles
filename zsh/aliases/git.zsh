bb10dfc1f8fabc165b4986396a736cd144e50081
John Costanzo <jrock2004@users.noreply.github.com>
2018-04-13 15:40:52 -0400

    This breaks it

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -1,4 +1,4 @@
 alias gco='git checkout'
-alias gcorb='gco --track $(git branch -rvv | fzf)'
-alias gcob='gco $(git branch -vv | fzf)'
+alias gcorb='gco --track $(git branch -r | fzf)'
+alias gcob='gco $(git branch | fzf)'
 alias gmb='git merge $(git branch | fzf)'
