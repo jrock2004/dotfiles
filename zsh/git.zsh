82a8860666e3e9034d8bdf22e842519df022c464
John Costanzo <jrock2004@gmail.com>
2019-05-13 20:17:18 -0400

    Adding missing gcorb

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,6 +1,8 @@
 # git aliases
 alias gs='git s'
 alias glog='git l'
+alias gcorb='git checkout --track $(git branch -r | fzf)'
+alias gcob='git checkout $(git branch | fzf)'
 
 function g() {
   if [[ $# > 0 ]]; then
