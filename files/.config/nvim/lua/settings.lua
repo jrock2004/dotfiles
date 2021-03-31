e9e13c900ba4c0c243ba64ed2a761567b560b82a
John Costanzo <jrock2004@gmail.com>
2021-03-30 22:48:38 -0400

    setting my settings for now

--- files/.config/nvim/lua/settings.lua
+++ files/.config/nvim/lua/settings.lua
@@ -1,3 +1,64 @@
-local cmd = vim.cmd
+local o = vim.o -- get/set options
+local wo = vim.wo -- window local options
+local bo = vim.bo -- buffer local options
+--local g = vim.g -- global variables
+local cmd = vim.cmd -- ex commands
 
+-- get/set options
+o.dir = '/tmp'
+o.smartcase = true
+o.laststatus = 2
+o.incsearch = true
+o.ignorecase = true
+o.scrolloff = 8
+o.timeoutlen = 3000
+o.ttimeoutlen = 100
+o.mouse = 'a'
+o.wildmenu = true
+o.showmode = false
+o.listchars = 'eol:¬,tab:>·,extends:>,precedes:<,space:·'
+o.hidden = true
+o.background = 'dark'
+o.syntax = 'on'
+o.backspace = 'indent,eol,start'
+o.clipboard = 'unnamedplus'
+o.autoread = true
+o.inccommand = 'nosplit'
+o.cmdheight = 2
+o.encoding = 'utf-8'
+o.termguicolors = true
+o.t_Co = '256'
+o.backup = false
+o.writebackup = false
+o.pumheight = 2
+o.splitbelow = true
+o.splitright = true
+o.showtabline = 2 -- Always show tabs
+o.updatetime = 50
+
+-- buffer local options
+bo.swapfile = false
+bo.shiftwidth = 2
+bo.softtabstop = 2
+bo.tabstop = 2
+bo.autoindent = false
+bo.expandtab = true
+bo.smartindent = true
+
+-- window local options
+wo.cursorline = false
+wo.foldenable = false
+wo.linebreak = false
+wo.relativenumber = true
+wo.signcolumn = "yes"
+wo.number = true
+wo.cursorline = true
+
+-- global variables
+
+-- ex commands
 cmd 'colorscheme nvcode' -- setting theme
+cmd 'set colorcolumn=99999' -- help to fix indentation
+cmd 'set iskeyword+=-' -- make it so - is part of the work
+cmd 'set shortmess+=c' -- don't pass messages to |ins-completion-menu|.
+
