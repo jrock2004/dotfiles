b8a55dc8bcc99671205506d3aaafaeb84ad7033e
John Costanzo <jrock2004@users.noreply.github.com>
2021-04-07 22:38:52 -0400

    new brew command to install

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ setup_homebrew() {
 		special_echo "Homebrew not installed. Installing."
 
 		# Run as a login shell (non-interactive) so that the script doesn't pause for user input
-		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 
 	if [ "$(uname)" == "Linux" ]; then
