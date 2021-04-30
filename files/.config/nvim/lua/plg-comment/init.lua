a0275740d8ce235a7a93cc8c2a40e21820189529
John Costanzo <jrock2004@gmail.com>
2021-04-30 09:00:42 -0400

    adding gcc support

--- files/.config/nvim/lua/plg-comment/init.lua
+++ files/.config/nvim/lua/plg-comment/init.lua
@@ -1,7 +1,11 @@
 local options = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
+local cmd = vim.cmd
 
 require('nvim_comment').setup()
 
 map('n', '<leader>gcc', ':CommentToggle<CR>', options)
 map('v', '<leader>gcc', ':CommentToggle<CR>', options)
+
+-- temp override to get gcc access
+cmd('nmap <expr> gcc (v:count? \':CommentToggle<CR>\' : \':set operatorfunc=CommentOperator<CR>g@\')')
