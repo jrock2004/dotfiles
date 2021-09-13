413aec621c782dbcde683d6e7f99c13335acfd63
John Costanzo <jrock2004@gmail.com>
2021-09-13 14:33:16 -0400

    feat: adding tmux config

---
+++ applescripts/tunes.js
@@ -0,0 +1,18 @@
+const symbol = "♫"
+let output = "";
+
+if (Application("Music").running()) {
+  const track = Application("Music").currentTrack,
+    artist = track.artist(),
+    title = track.name();
+
+  output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+} else if (Application("Spotify").running()) {
+  const track = Application("Spotify").currentTrack,
+    artist = track.artist(),
+    title = track.name();
+
+  output = `${symbol} ${title} - ${artist}`.substr(0, 50);
+}
+
+output;
