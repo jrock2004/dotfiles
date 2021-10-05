963d5f9fb2f8defdad11dd058e27050cb5eee55f
John Costanzo <jrock2004@gmail.com>
2021-10-05 13:05:24 -0400

    fix: fixing nvimtree settings

--- files/.config/nvim/lua/plugins/nvimtree.lua
+++ files/.config/nvim/lua/plugins/nvimtree.lua
@@ -3,12 +3,41 @@ local nmap = keymap.nmap
 local g = vim.g
 -- local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
--- plugin settings
-g.nvim_tree_auto_close = 1
-g.nvim_tree_auto_ignore_ft = 'startify'
-g.nvim_tree_follow = 1
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 0
+require'nvim-tree'.setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {'startify'},
+  auto_close = true,
+  open_on_tab = false,
+  -- update_to_buf_dir = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
+  hijack_cursor = false,
+  update_cwd = false,
+  lsp_diagnostics = false,
+  update_focused_file = {
+    enable = false,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  view = {
+    width = 30,
+    height = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {},
+    }
+  }
+}
+
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 
 nmap('<leader>-', ':NvimTreeToggle<CR>')
