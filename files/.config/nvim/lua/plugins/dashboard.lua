6d7c17923ad592914a8857973f948851cf33a7d0
John Costanzo <jrock2004@gmail.com>
2021-11-29 14:41:45 -0500

    chore: adding git status

--- files/.config/nvim/lua/plugins/dashboard.lua
+++ files/.config/nvim/lua/plugins/dashboard.lua
@@ -1,6 +1,27 @@
 local g = vim.g
 local cmd = vim.cmd
 local icons = Lvim.icons
+local utils = require('telescope.utils')
+local set_var = vim.api.nvim_set_var
+
+local git_root, ret = utils.get_os_command_output({ "git", "rev-parse", "--show-toplevel" }, vim.loop.cwd())
+
+local function get_dashboard_git_status()
+  local git_cmd = {'git', 'status', '-s', '--', '.'}
+  local output = utils.get_os_command_output(git_cmd)
+  set_var('dashboard_custom_footer', {'Git status', '', unpack(output)})
+end
+
+if ret ~= 0 then
+  local is_worktree = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" }, vim.loop.cwd())
+  if is_worktree[1] == "true" then
+    get_dashboard_git_status()
+  else
+    set_var('dashboard_custom_footer', {'Not in a git directory'})
+  end
+else
+    get_dashboard_git_status()
+end
 
 -- g.dashboard_custom_header = {
 --   '    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀  ',
@@ -25,22 +46,23 @@ local icons = Lvim.icons
 --   '    ⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘  ',
 -- }
 
-g.dashboard_custom_header = {
-  '⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀',
-  '⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀⡏⠀⠀⠀⠀⠀⢷ ',
-  '⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇',
-  '⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀⠀OK⠀ ⡇',
-  '⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇',
-  '⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼ ',
-  '⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀ ',
-  '⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-  '⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀',
-  '⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀',
-  '⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀',
-  '⣿⣿⣧⣀⣿………⣀⣰⣏⣘⣆⣀⠀⠀       '
-}
+-- g.dashboard_custom_header = {
+--   '⠀⠀⠀⠀⣠⣶⡾⠏⠉⠙⠳⢦⡀⠀⠀⠀⢠⠞⠉⠙⠲⡀⠀',
+--   '⠀⠀⠀⣴⠿⠏⠀⠀⠀⠀⠀⠀⢳⡀⠀⡏⠀⠀⠀⠀⠀⢷ ',
+--   '⠀⠀⢠⣟⣋⡀⢀⣀⣀⡀⠀⣀⡀⣧⠀⢸⠀⠀⠀⠀⠀ ⡇',
+--   '⠀⠀⢸⣯⡭⠁⠸⣛⣟⠆⡴⣻⡲⣿⠀⣸⠀⠀OK⠀ ⡇',
+--   '⠀⠀⣟⣿⡭⠀⠀⠀⠀⠀⢱⠀⠀⣿⠀⢹⠀⠀⠀⠀⠀ ⡇',
+--   '⠀⠀⠙⢿⣯⠄⠀⠀⠀⢀⡀⠀⠀⡿⠀⠀⡇⠀⠀⠀⠀⡼ ',
+--   '⠀⠀⠀⠀⠹⣶⠆⠀⠀⠀⠀⠀⡴⠃⠀⠀⠘⠤⣄⣠⠞⠀ ',
+--   '⠀⠀⠀⠀⠀⢸⣷⡦⢤⡤⢤⣞⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+--   '⠀⠀⢀⣤⣴⣿⣏⠁⠀⠀⠸⣏⢯⣷⣖⣦⡀⠀⠀⠀⠀⠀⠀',
+--   '⢀⣾⣽⣿⣿⣿⣿⠛⢲⣶⣾⢉⡷⣿⣿⠵⣿⠀⠀⠀⠀⠀⠀',
+--   '⣼⣿⠍⠉⣿⡭⠉⠙⢺⣇⣼⡏⠀⠀⠀⣄⢸⠀⠀⠀⠀⠀⠀',
+--   '⣿⣿⣧⣀⣿………⣀⣰⣏⣘⣆⣀⠀⠀       ',
+-- }
 
-g.dashboard_default_executive = 'telescope'
+g.dashboard_custom_header = {''}
+g.dashboard_default_executive = {'telescope'}
 g.dashboard_custom_section = {
   a = {description = {icons.fileNoBg .. 'Find File          '}, command = 'Telescope find_files hidden=true'},
   b = {description = {icons.fileCopy .. 'Recently Used Files'}, command = 'Telescope oldfiles hidden=true'},
