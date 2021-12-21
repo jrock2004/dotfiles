7ebaae932c30da4f42f6608eae3b590fb07336f4
John Costanzo <jrock2004@gmail.com>
2021-12-21 11:56:54 -0500

    chore: adding cool plugin for helping writing in lua

--- files/.config/nvim/lua/user/plugins.lua
+++ files/.config/nvim/lua/user/plugins.lua
@@ -73,6 +73,7 @@ return packer.startup(function(use)
 	use("hrsh7th/cmp-cmdline") -- cmdline completions
 	use("saadparwaiz1/cmp_luasnip") -- snippet completions
 	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua") -- helping with writing in lua
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
