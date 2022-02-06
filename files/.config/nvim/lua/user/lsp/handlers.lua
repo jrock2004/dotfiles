490866ed4cb08ea0218d11de1813d19f80d599fc
John Costanzo <jrock2004@gmail.com>
2022-02-05 22:27:40 -0500

    chore: fixing null-ls for html files. Fixing linux apps to install

--- files/.config/nvim/lua/user/lsp/handlers.lua
+++ files/.config/nvim/lua/user/lsp/handlers.lua
@@ -85,7 +85,9 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
+	if client.name == "html" then
+		client.resolved_capabilities.document_formatting = false
+	elseif client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
 	elseif client.name == "jsonls" then
 		client.resolved_capabilities.document_formatting = false
--- linux.sh
+++ linux.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:lazygit-team/release
 sudo apt-get update
 
-sudo apt-get install alacritty highlight cabextract xclip ack bat cloc cmake fzf gcc git gnupg grep htop i3-wm i3lock i3status jq luarocks make markdown ninja-build neofetch neovim python ripgrep rofi shellcheck stow tmux tree vim wget zsh
+# sudo apt-get install alacritty highlight cabextract xclip ack bat cloc cmake fzf gcc git gnupg grep htop jq luarocks make markdown ninja-build neofetch neovim python ripgrep rofi shellcheck stow tmux tree vim wget zsh
+
+sudo apt-get install \
+  alacritty \
+  ack \
+  bat \
+  cabextract \
+  cloc \
+  cmake \
+  fzf \
+  gcc \
+  git \
+  gnupg \
+  golang \
+  grep \
+  htop \
+  jq \
+  lazygit \
+  luarocks \
+  make \
+  markdown \
+  neofetch \
+  neovim \
+  ninja-build \
+  python \
+  ripgrep \
+  shellcheck \
+  stow \
+  tmux \
+  tree \
+  vim \
+  wget \
+  xclip \
+  zsh
+
+# Nice prompt shell
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
