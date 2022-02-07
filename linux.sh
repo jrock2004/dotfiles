f1185bd325cd77abfbc880e2b806d481a93b1a0e
John Costanzo <jrock2004@gmail.com>
2022-02-07 09:57:53 -0500

    chore: updating to working fonts for kitty and alacritty

--- linux.sh
+++ linux.sh
@@ -3,40 +3,12 @@
 # sudo add-apt-repository ppa:neovim-ppa/unstable
 # sudo apt-get update
 
-# sudo apt-get install \
-#   alacritty \
-#   ack \
-#   bat \
-#   cabextract \
-#   cloc \
-#   cmake \
-#   fzf \
-#   gcc \
-#   git \
-#   gnupg \
-#   golang \
-#   grep \
-#   htop \
-#   jq \
-#   luarocks \
-#   make \
-#   markdown \
-#   neofetch \
-#   neovim \
-#   ninja-build \
-#   ripgrep \
-#   shellcheck \
-#   stow \
-#   tmux \
-#   tree \
-#   vim \
-#   wget \
-#   xclip \
-#   zsh
-
 sudo apt-get install \
   alacritty \
-  cabextract
+  cabextract \
+  libx11-dev \
+  libxinerama-dev \
+  libxft-dev
 
 # Nice prompt shell
 # sh -c "$(curl -fsSL https://starship.rs/install.sh)"
