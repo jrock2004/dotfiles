988674d908b5f9757988cfd2ee71bc081e9afab8
John Costanzo <jrock2004@gmail.com>
2022-04-04 10:29:13 -0400

    chore: fixing snippets

--- files/.config/nvim/lua/user/cmp.lua
+++ files/.config/nvim/lua/user/cmp.lua
@@ -112,8 +112,9 @@ cmp.setup({
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
-				nvim_lua = "[NVIM_LUA]",
+				-- nvim_lua = "[NVIM_LUA]",
 				luasnip = "[Snippet]",
+				copilot = "[Copilot]",
 				buffer = "[Buffer]",
 				path = "[Path]",
 			})[entry.source.name]
@@ -122,7 +123,7 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
+		-- { name = "nvim_lua" },
 		{ name = "luasnip" },
 		{ name = "copilot" },
 		{ name = "buffer" },
--- files/.config/nvim/lua/user/plugins.lua
+++ files/.config/nvim/lua/user/plugins.lua
@@ -78,7 +78,7 @@ return packer.startup(function(use)
 
 	-- snippets
 	use("L3MON4D3/LuaSnip") --snippet engine
-	-- use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
