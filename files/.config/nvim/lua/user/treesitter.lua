0cd57f336db66dcd830f329bb5305dc954415f0c
John Costanzo <jrock2004@gmail.com>
2022-04-25 09:00:36 -0400

    chore: fixing issues with treesitter

--- files/.config/nvim/lua/user/treesitter.lua
+++ files/.config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,30 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"bash",
+		"c_sharp",
+		"comment",
+		"css",
+		"dockerfile",
+		"graphql",
+		"html",
+		"http",
+		"javascript",
+		"jsdoc",
+		"json",
+		"json5",
+		"lua",
+		"markdown",
+		"ninja",
+		"regex",
+		"scss",
+		"svelte",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	autopairs = {
