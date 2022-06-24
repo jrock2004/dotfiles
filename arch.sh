4da4d56427b2d55b3ce3c4e4b3b8003280daabb6
John Costanzo <jrock2004@gmail.com>
2022-06-24 16:20:02 -0400

    chore: adding things to ignore file and adding missing items to install on arch

--- arch.sh
+++ arch.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -S discord
 
-yay -S ack alacritty bat cloc cmake diff-so-fancy dmenu fd fzf gcc github-cli gnupg go google-chrome grep htop jq lazygit luarocks make markdown neofetch neovim ninja nitrogen orchis-theme-git picom python python-pip ripgrep shellcheck slack-desktop starship stow stylua tela-icon-theme tmux tree vim wget xmonad xmonad-contrib xmobar xterm z zsh
+yay -S ack alacritty bat bluez bluez-utils cloc cmake diff-so-fancy dmenu fd fzf gcc github-cli gnupg go google-chrome grep htop jq lazygit luarocks make markdown neofetch neovim ninja nitrogen openssh orchis-theme-git polybar picom python python-pip ripgrep sddm shellcheck slack-desktop starship stow stylua tela-icon-theme tmux tree vim wget xclip xmonad xmonad-contrib xmobar xsel xterm z zsh
 
 pip install git+https://github.com/psf/black
