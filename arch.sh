9ed9ede0e351ece840b0571b8bea5c260b34d031
John Costanzo <jrock2004@gmail.com>
2022-06-24 13:52:15 -0400

    chore: adding some missing items

--- arch.sh
+++ arch.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -S discord
 
-yay -S ack alacritty bat cloc cmake diff-so-fancy fd fzf gcc github-cli gnupg go google-chrome grep htop jq lazygit luarocks make markdown neofetch neovim ninja orchis-theme-git python python-pip ripgrep shellcheck slack-desktop starship stow stylua tela-icon-theme tmux tree vim wget z zsh
+yay -S ack alacritty bat cloc cmake diff-so-fancy dmenu fd fzf gcc github-cli gnupg go google-chrome grep htop jq lazygit luarocks make markdown neofetch neovim ninja nitrogen orchis-theme-git picom python python-pip ripgrep shellcheck slack-desktop starship stow stylua tela-icon-theme tmux tree vim wget xmonad xmonad-contrib xmobar xterm z zsh
 
 pip install git+https://github.com/psf/black
