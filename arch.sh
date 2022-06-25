d1fe7a4d216d48d99304656419f644d2a2ea6ae7
John Costanzo <jrock2004@gmail.com>
2022-06-25 16:54:57 -0400

    feat: asking questions for arch

--- arch.sh
+++ arch.sh
@@ -1,10 +1,47 @@
 #!/usr/bin/env bash
 
-# sudo pacman -S discord pcmanfm pavucontrol xautolock pipewire-pulse pipewire-media-session virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq
-sudo pacman -S discord pcmanfm xautolock virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq pulseaudio-bluetooth
+sudo pacman -S discord virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq 
 
 # Need to figure out markdown
 
-yay -S ack alacritty bat bluez bluez-utils cloc cmake diff-so-fancy dmenu fd fzf gcc github-cli gnupg go gpicview google-chrome grep htop jq lazygit luarocks make neofetch neovim noto-fonts-emoji ninja nitrogen openssh orchis-theme-git polybar picom python python-pip ripgrep sddm shellcheck slack-desktop spotify starship stow stylua tela-icon-theme tmux tree vim wget xclip xmonad xmonad-contrib xmobar xorg-xmessage xsel xterm z zsh
+yay -S ack alacritty bat bluez bluez-utils cloc cmake diff-so-fancy fd fzf gcc github-cli gnupg go google-chrome grep htop jq lazygit luarocks make neofetch neovim noto-fonts-emoji ninja nitrogen openssh python python-pip ripgrep shellcheck slack-desktop spotify starship stow stylua tmux tree vim wget xclip xsel xterm z zsh
 
 pip install git+https://github.com/psf/black
+
+echo -e "Are we installing a Windows Manager? \n\n"
+
+USINGWM=false
+
+select isWM in yes no; do
+  case $isWM in
+    yes)
+      USINGWM=true
+
+      yay -S orchis-theme-git tela-icon-theme
+
+      break ;;
+    no)
+      break ;;
+    *)
+      error "Invalid option $REPLY"
+  esac
+done
+
+if [ "$USINGWM" == false ]; then
+  yay -S dmenu gpicview pcmanfm polybar picom pulseaudio-bluetooth xautolock xmonad xmonad-contrib xmobar xorg-xmessage
+fi
+
+echo -e "Do we still want to install a tiling manager? \n\n"
+
+select isTM in yes no; do
+  case $isTM in
+    yes)
+      yay -S dmenu gpicview polybar picom xmonad xmonad-contrib xmobar xorg-xmessage
+
+      break ;;
+    no)
+      break ;;
+    *)
+      error "Invalid option $REPLY"
+  esac
+done
