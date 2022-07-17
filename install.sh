919a3cb507e20158adfd13a79be77ac9af781292
John Costanzo <jrock2004@gmail.com>
2022-07-17 16:52:59 -0400

    ci: y not being seen

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ setup_init() {
   fi
 
   if [ "$OS" == "arch" ]; then
-    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSfy | sh
+    curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf -y | sh
 
     if [ -z "$(command -v paru)" ]; then
       info "You need to install paru before you can run this script"
