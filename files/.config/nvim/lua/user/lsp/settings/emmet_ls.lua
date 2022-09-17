98ee490e7b208ae18eeb7400b3c459f4ef4de4a2
John Costanzo <jrock2004@gmail.com>
2022-09-17 14:58:56 -0400

    chore: trying to get eslint config when npm installed

--- files/.config/nvim/lua/user/lsp/settings/emmet_ls.lua
+++ files/.config/nvim/lua/user/lsp/settings/emmet_ls.lua
@@ -1,25 +1,47 @@
+local function eslint_config()
+	local f = io.open("package.json", "r")
+
+	if f ~= nil then
+		local package = f:read("*all")
+
+		if package:find('"eslintConfig"') then
+			io.close(f)
+			return "package.json"
+		end
+
+		io.close(f)
+
+		return "package.json"
+	end
+
+	return nil
+end
+
 -- https://github.com/pedro757/emmet
 -- npm i -g ls_emmet
 return {
-  cmd = { "ls_emmet", "--stdio" },
-  filetypes = {
-    "html",
-    "css",
-    "scss",
-    "javascript",
-    "javascriptreact",
-    "typescript",
-    "typescriptreact",
-    "haml",
-    "xml",
-    "xsl",
-    "pug",
-    "slim",
-    "sass",
-    "stylus",
-    "less",
-    "sss",
-    "hbs",
-    "handlebars",
-  },
+	cmd = { "ls_emmet", "--stdio" },
+	filetypes = {
+		"html",
+		"css",
+		"scss",
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"haml",
+		"xml",
+		"xsl",
+		"pug",
+		"slim",
+		"sass",
+		"stylus",
+		"less",
+		"sss",
+		"hbs",
+		"handlebars",
+	},
+	options = {
+		overrideConfigFile = eslint_config,
+	},
 }
