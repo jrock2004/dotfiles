1a614862295b3f260e05fc25b26b28bd9eba811f
John Costanzo <jrock2004@gmail.com>
2023-01-03 18:08:19 -0500

    chore: working on transparent bg

--- files/.config/lvim/config.lua
+++ files/.config/lvim/config.lua
@@ -13,6 +13,7 @@ lvim.log.level = "warn"
 lvim.format_on_save = { timeout = 2000 }
 lvim.format_on_save.enabled = true
 lvim.colorscheme = "lunar"
+lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
