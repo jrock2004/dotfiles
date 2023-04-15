a52c67ffdffeff37ec8e8868a12a97244b220e6a
John Costanzo <jrock2004@users.noreply.github.com>
2023-04-15 16:36:40 -0400

    Finish neovim install

--- install.sh
+++ install.sh
@@ -194,6 +194,7 @@ installAppsForDebian() {
   # Neovim
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
+  sudo mv nvim.appimage /usr/local/bin/
 }
 
 installAppsForMac() {
