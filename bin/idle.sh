be28731caee360c91d8267e90cfd2550eea30d8b
John Costanzo <jrock2004@gmail.com>
2023-06-19 17:13:37 -0400

    feat: adding binaries I like to run

---
+++ bin/idle.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+swayidle \
+    timeout 300 "$HOME/.dotfiles/bin/lock.sh --grace 10 --fade-in 0" \
+    timeout 540 'hyprctl dispatch dpms off DP-2' \
+    resume 'hyprctl dispatch dpms on DP-2'
+# before-sleep "$HOME/.dotfiles/bin/lock.sh --fade-in 0"
