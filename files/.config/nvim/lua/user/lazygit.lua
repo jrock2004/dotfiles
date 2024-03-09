c81cc023e1c830d21ef0ae9e4047154f0672fb44
John Costanzo <jrock2004@gmail.com>
2024-03-08 21:34:09 -0500

    fixing lazygit

--- files/.config/nvim/lua/user/lazygit.lua
+++ files/.config/nvim/lua/user/lazygit.lua
@@ -13,4 +13,16 @@ local M = {
   },
 }
 
+function M.config()
+  local wk = require 'which-key'
+
+  wk.register {
+    ['<leader>gg'] = { '<cmd>LazyGit<cr>', 'LazyGit' },
+    ['<leader>gc'] = { '<cmd>LazyGitConfig<cr>', 'LazyGitConfig' },
+    ['<leader>gf'] = { '<cmd>LazyGitCurrentFile<cr>', 'LazyGitCurrentFile' },
+    ['<leader>gF'] = { '<cmd>LazyGitFilter<cr>', 'LazyGitFilter' },
+    ['<leader>gC'] = { '<cmd>LazyGitFilterCurrentFile<cr>', 'LazyGitFilterCurrentFile' },
+  }
+end
+
 return M
