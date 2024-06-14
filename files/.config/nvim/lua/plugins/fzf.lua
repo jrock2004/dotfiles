a7a641a64a09e4b6e9102adf7e96f7104d3e1c76
John Costanzo <jrock2004@gmail.com>
2024-06-14 16:24:58 -0400

    switching to fzf file find

---
+++ files/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,10 @@
+return {
+  "ibhagwan/fzf-lua",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>,", false },
+    { "<leader><space>", false },
+    { "<leader>sf", LazyVim.pick("auto", { root = false }), desc = "Find Files (Root Dir)" },
+    { "<leader>sF", LazyVim.pick("auto", { root = true }), desc = "Find Files (cwd)" },
+  },
+}
