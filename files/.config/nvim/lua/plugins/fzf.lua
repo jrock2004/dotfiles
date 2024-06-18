dd6904854adc5af2ed92a09b0bf65e25ebf07ecd
John Costanzo <jrock2004@gmail.com>
2024-06-17 20:51:56 -0400

    upgrade dep

--- files/.config/nvim/lua/plugins/fzf.lua
+++ files/.config/nvim/lua/plugins/fzf.lua
@@ -6,5 +6,6 @@ return {
     { "<leader><space>", false },
     { "<leader>sf", LazyVim.pick("auto", { root = false }), desc = "Find Files (Root Dir)" },
     { "<leader>sF", LazyVim.pick("auto", { root = true }), desc = "Find Files (cwd)" },
+    -- { "<leader>sf", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
   },
 }
