c2248a78fc27d87352afe6f500e382b06835dfcc
John Costanzo <jrock2004@gmail.com>
2024-11-17 14:59:36 -0500

    adding neotest

--- files/.config/nvim/lua/plugins/neotest.lua
+++ files/.config/nvim/lua/plugins/neotest.lua
@@ -2,11 +2,18 @@ return {
   "nvim-neotest/neotest",
   dependencies = {
     "haydenmeade/neotest-jest",
+    "marilari88/neotest-vitest",
   },
-  opts = function(_, opts)
-    table.insert(
-      opts.adapters,
-      require("neotest-jest")({
+  opts = {
+    adapters = {
+      ["neotest-vitest"] = {
+        vitestCommand = "pnpx vitest",
+        env = { CI = true },
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+      },
+      ["neotest-jest"] = {
         -- jestCommand = 'npm test --',
         jestCommand = "pnpm test --",
         -- jestCommand = "yarn test",
@@ -15,7 +22,7 @@ return {
         cwd = function()
           return vim.fn.getcwd()
         end,
-      })
-    )
-  end,
+      },
+    },
+  },
 }
