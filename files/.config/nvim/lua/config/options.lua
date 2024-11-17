3ed5e6233f3898a14efde4ae188459e4d035f1bb
John Costanzo <jrock2004@gmail.com>
2024-11-17 15:42:01 -0500

    update to new lazyvim

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 vim.cmd("autocmd BufRead *.dockerfile* set filetype=dockerfile")
 vim.cmd("autocmd BufRead *.env* set filetype=sh")
 vim.cmd("autocmd BufRead *.handlebars set filetype=handlebars")
