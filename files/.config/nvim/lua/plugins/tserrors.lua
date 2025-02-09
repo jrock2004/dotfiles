c90e927de2756d1a9a16b4d3219937ca61314954
John Costanzo <jrock2004@gmail.com>
2025-02-08 21:02:24 -0500

    updating lazyvim and fixing errors

--- files/.config/nvim/lua/plugins/tserrors.lua
+++ files/.config/nvim/lua/plugins/tserrors.lua
@@ -1,7 +1,7 @@
 return {
   "OlegGulevskyy/better-ts-errors.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
-  config = {
+  opts = {
     keymaps = {
       toggle = "<leader>dd", -- default '<leader>dd'
       go_to_definition = "<leader>dx", -- default '<leader>dx'
