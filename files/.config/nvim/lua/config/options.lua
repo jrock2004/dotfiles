9146fde8286cc32922079792fbef44c5a2098a63
John Costanzo <jrock2004@gmail.com>
2026-01-06 17:58:53 -0500

    adding some of my options and keymaps

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.cmd("autocmd BufRead *.dockerfile* set filetype=dockerfile")
+vim.cmd("autocmd BufRead *.env* set filetype=sh")
+vim.cmd("autocmd BufRead *.handlebars set filetype=handlebars")
+
+vim.g.lazyvim_eslint_auto_format = true
